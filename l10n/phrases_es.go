d07d3fca79860369624756da14bcb0ce73f65fa5
Matt Baer <matt@write.as>
2018-02-05 02:19:41 -0500

    Add Spanish localization

---
+++ l10n/phrases_es.go
@@ -0,0 +1,24 @@
+package l10n
+
+var phrasesES = map[string]string{
+	"Anonymous post": "Entrada anónima",
+	"Blogs":          "Blogs",
+	"Newer":          "Más nuevo",
+	"Older":          "Más antiguo",
+	"Posts":          "Entradas",
+	"Publish to...":  "Publicar en...",
+	"Publish":        "Publicar",
+	"Read more...":   "Leer más...",
+	"Toggle theme":   "Cambiar tema",
+	"View posts":     "Ver entradas",
+	"delete":         "eliminar",
+	"edit":           "editar",
+	"move to...":     "mover a",
+	"pin":            "anclar",
+	"published with write.as":  "publicado con write.as",
+	"share modal ending":       "Enviar a un amigo, compartir a través de la web, o quizá tuitear la entrada. Aprende más.",
+	"share modal introduction": "Cada entrada publicada tiene una secreta y única URL que puedes compartir con cualquiera. Esta es la URL:",
+	"share modal title":        "Compartir esta entrada",
+	"share":                    "compartir",
+	"unpin":                    "desanclar",
+}
