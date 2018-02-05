2e97d00a6140b9e6734c793e3191a33d87c1da59
Matt Baer <matt@write.as>
2018-02-05 00:55:39 -0500

    Add Hungarian localization

---
+++ l10n/phrases_hu.go
@@ -0,0 +1,24 @@
+package l10n
+
+var phrasesHU = map[string]string{
+	"Anonymous post": "Névtelen bejegyzés",
+	"Blogs":          "Blogok",
+	"Newer":          "Újabb",
+	"Older":          "Régebbi",
+	"Posts":          "Bejegyzések",
+	"Publish to...":  "Publikálja ide...",
+	"Publish":        "Publikálás",
+	"Read more...":   "Olvass tovább...",
+	"Toggle theme":   "Szín váltása",
+	"View posts":     "Bejegyzések megtekintése",
+	"delete":         "törlés",
+	"edit":           "szerkesztés",
+	"move to...":     "költözd át...",
+	"pin":            "rögzítés",
+	"published with write.as":  "közzétett write.as segitségével",
+	"share modal ending":       "Küldje el egy ismerősnek, ossza meg az interneten keresztül, akár Twitteren. További információ:",
+	"share modal introduction": "Minden közzétett bejegyzés egy titkos, egyedi URL-címet kap, amit maga megoszthat bárkivel. Az URL-cím:",
+	"share modal title":        "Ossza meg ezt a bejegyzést",
+	"share":                    "megosztás",
+	"unpin":                    "rögzítés felbontása",
+}
--- l10n/strings.go
+++ l10n/strings.go
@@ -2,6 +2,8 @@ package l10n
 
 func Strings(lang string) map[string]string {
 	switch lang {
+	case "hu":
+		return phrasesHU
 	default:
 		return phrases
 	}
