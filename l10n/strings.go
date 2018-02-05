5e9c5baf355f0f933c9bbd9b16c0d2994171fa87
Matt Baer <matt@write.as>
2018-02-05 01:24:32 -0500

    Document Strings() func

--- l10n/strings.go
+++ l10n/strings.go
@@ -1,5 +1,7 @@
 package l10n
 
+// Strings returns a translation set that will take any term and return its
+// translation.
 func Strings(lang string) map[string]string {
 	switch lang {
 	case "hu":
