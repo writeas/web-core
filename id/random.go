95a3a717ed8f5ed245147a1b214218a8ad904098
Matt Baer <matt@write.as>
2021-03-30 12:44:22 -0400

    Move remaining writeas/nerds/store funcs to web-core
    Finishes the work started in #8.

--- id/random.go
+++ id/random.go
@@ -1,8 +1,8 @@
 package id
 
 import (
-	"fmt"
 	"crypto/rand"
+	"fmt"
 )
 
 // GenerateRandomString creates a random string of characters of the given
@@ -23,4 +23,16 @@ func GenerateRandomString(dictionary string, l int) string {
 // original slug. It's "safe" because it uses 0-9 b-z excluding vowels.
 func GenSafeUniqueSlug(slug string) string {
 	return fmt.Sprintf("%s-%s", slug, GenerateRandomString("0123456789bcdfghjklmnpqrstvwxyz", 4))
+}
+
+// Generate62RandomString creates a random string with the given length
+// consisting of characters in [A-Za-z0-9].
+func Generate62RandomString(l int) string {
+	return GenerateRandomString("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", l)
+}
+
+// GenerateFriendlyRandomString creates a random string of characters with the
+// given length consisting of characters in [a-z0-9].
+func GenerateFriendlyRandomString(l int) string {
+	return GenerateRandomString("0123456789abcdefghijklmnopqrstuvwxyz", l)
 }
